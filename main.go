3fd51048a41536e0d7463a71b2d6704f597e970b
ItohKaori <k197781@kansai-u.ac.jp>
2019-03-31 05:01:19 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	// $GOPATH/srcからのパスで指定する
+	"bookshelf/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// controllerパッケージから読み込む
+	router.GET("/", controller.IndexGET)
+	router.Run(":8080")
+}
