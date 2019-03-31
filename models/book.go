539901592c25d8e0b8f456f3921c08773d78d97c
ItohKaori <k197781@kansai-u.ac.jp>
2019-03-31 20:11:46 +0900

    change type

--- models/book.go
+++ models/book.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Book struct {
-	Id int64
-	Title string `sql:"size:255"`
-	Author string `sql:"size:255"`
-	Publisher string `sql:"size:255"`
-	PublishedDate string `sql:"size:255"`
+	Id int64 `gorm:"primary_key" json:"id"`
+	Title string `sql:"size:255" json:"title"`
+	Author string `sql:"size:255" json:"author"`
+	Publisher string `sql:"size:255" json:"publisher"`
+	PublishedDate time.Time `json:"published_date"`
 	CreatedAt time.Time
 	UpdatedAT time.Time
-	DeletedAt time.Time
+	DeletedAt *time.Time `sql:"index" json:"-"`
 }
